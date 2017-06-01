8765ebd81f2b302fcae0bb72ae9decb647bf9466
admin <ganluo@liandianyi.com>
2017-06-01 13:54:59 +0800

    1.1版本

--- MyGitTest/app/src/main/java/com/tianxi/mygittest/MainActivity.java
+++ MyGitTest/app/src/main/java/com/tianxi/mygittest/MainActivity.java
@@ -3,6 +3,9 @@ package com.tianxi.mygittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 1.1版本
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
